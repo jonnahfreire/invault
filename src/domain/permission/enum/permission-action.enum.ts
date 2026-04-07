09b31ab7df610d846062e13308cc6296bf334dff
= <=>
2026-04-06 22:34:50 -0300

    add permission enums

---
+++ src/domain/permission/enum/permission-action.enum.ts
@@ -0,0 +1,8 @@
+export enum PermissionAction {
+  READ = "read",
+  CREATE = "create",
+  UPDATE = "update",
+  DELETE = "delete",
+  ROTATE = "rotate",
+  GRANT = "grant",
+}
---
+++ src/domain/permission/enum/permission-resource.enum.ts
@@ -0,0 +1,9 @@
+export enum PermissionResource {
+  SYSTEM = "system",
+  ORGANIZATION = "organization",
+  PROJECT = "project",
+  APPLICATION = "application",
+  SECRET = "secret",
+  ROLE = "role",
+  USER = "user",
+}
